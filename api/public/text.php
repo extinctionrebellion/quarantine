8e5f06c3baead8978b2ab98c9c80d41ba23854cc
Daniel Sum <daniel@lacerisenumerique.be>
2020-03-15 20:34:12 +0100

    - Add Fiche EN/FR/NL - add temp localization files

---
+++ api/public/text.php
@@ -0,0 +1,63 @@
+<?php
+
+$file = '{
+    "Envie de donner un coup de main à votre voisin ?" : "Envie de donner un coup de main à votre voisin ?",
+  "Rejoignez notre mouvement et proposez à votre voisin \nde faire ses courses de première nécessité." : "Rejoignez notre mouvement et proposez à votre voisin \nde faire ses courses de première nécessité.",
+  "Participer" : "Participer",
+  "Répandons la solidarité, pas le virus" : "Répandons la solidarité, pas le virus",
+  "Pour endiguer rapidement la propagation du virus, le gouvernement et le milieu médical recommandent fortement à la population de rester à domicile. Mais comment faire pour s’alimenter et se fournir en produits de première nécessité tout en restant chez soi ?" : "Pour endiguer rapidement la propagation du virus, le gouvernement et le milieu médical recommandent fortement à la population de rester à domicile. Mais comment faire pour s’alimenter et se fournir en produits de première nécessité tout en restant chez soi ?",
+  "Comment ça marche ?" : "Comment ça marche ?",
+  "Je souhaite obtenir de l’aide" : "Je souhaite obtenir de l’aide",
+  "Imprimez la liste de course ou demandez à votre voisin de le faire pour vous." : "Imprimez la liste de course ou demandez à votre voisin de le faire pour vous.",
+  "Postez-la sur à votre fenêtre, collez-la à votre porte ou placez-la dans votre hall d’entrée." : "Postez-la sur à votre fenêtre, collez-la à votre porte ou placez-la dans votre hall d’entrée.",
+  "Attendez qu’un voisin la récupère. Il prendra contact avec vous et vous livrera sans tarder." : "Attendez qu’un voisin la récupère. Il prendra contact avec vous et vous livrera sans tarder.",
+  "Je suis un bénévole et je veux aider" : "Je suis un bénévole et je veux aider",
+  "Imprimez le modèle “liste de course” en format A4" : "Imprimez le modèle “liste de course” en format A4",
+  "Partagez-la ou placez-la dans la boite aux lettre de votre voisin" : "Partagez-la ou placez-la dans la boite aux lettre de votre voisin",
+  "Retournez chez vous et discutez avec vos proches autour d’un café." : "Retournez chez vous et discutez avec vos proches autour d’un café.",
+  "Baladez-vous dans votre rue pour voir qui vous pourriez aider" : "Baladez-vous dans votre rue pour voir qui vous pourriez aider",
+  "Récupérez la liste de courses et prévenez votre voisin !" : "Récupérez la liste de courses et prévenez votre voisin !",
+  "Rendez-vous en magasin et ne dévalisez pas les étagères." : "Rendez-vous en magasin et ne dévalisez pas les étagères.s",
+  "Retournez délivrer les produits devant la porte et sonnez à la porte !" : "Retournez délivrer les produits devant la porte et sonnez à la porte !",
+  "Mission réussie, merci infiniment pour votre collaboration !" : "Mission réussie, merci infiniment pour votre collaboration !",
+  "Partagez une photo sur Facebook avec le #covidsolidarity" : "Partagez une photo sur Facebook avec le #covidsolidarity",
+  "Vous avez des questions ? Nous avons les réponses !" : "Vous avez des questions ? Nous avons les réponses !",
+  "Je suis un bénévole et je veux aider" : "Je suis un bénévole et je veux aider",
+  "Suis-je rémunéré pour aider en aide à mon voisin ?" : "Suis-je rémunéré pour aider en aide à mon voisin ?",
+  "Non, nous agissons dans une approche strictement citoyenne et bénévole." : "Non, nous agissons dans une approche strictement citoyenne et bénévole.",
+  "Mais libre à votre voisin de vous remercier à sa guise s’il le souhaite." : "Mais libre à votre voisin de vous remercier à sa guise s’il le souhaite.",
+  "Je ne veux pas avancer de l’argent" : "Je ne veux pas avancer de l’argent",
+  "C’est une condition indispensable pour aider un voisin. Pour alléger votre portefeuille, nous vous invitons à n’acheter que des biens de première nécessité." : "C’est une condition indispensable pour aider un voisin. Pour alléger votre portefeuille, nous vous invitons à n’acheter que des biens de première nécessité.",
+  "Comment déposer les courses au sein d’un immeuble ?" : "Comment déposer les courses au sein d’un immeuble ?",
+  "Il suffit de prendre contact avec votre voisin pour lui indiquer l’heure à laquelle vous déposerez les courses devant sa porte. N’oubliez pas de respecter les règles d’hygiène." : "Il suffit de prendre contact avec votre voisin pour lui indiquer l’heure à laquelle vous déposerez les courses devant sa porte. N’oubliez pas de respecter les règles d’hygiène.",
+  "Comment vais-je être remboursé ?" : "Comment vais-je être remboursé ?",
+  "C’est très simple ! Il suffit d’indiquer votre numéro de compte sur la liste de course que vous rendrez à votre voisin. Pour le bon service rendu, il vous remboursera sans tarder." : "C’est très simple ! Il suffit d’indiquer votre numéro de compte sur la liste de course que vous rendrez à votre voisin. Pour le bon service rendu, il vous remboursera sans tarder.",
+  "Je souhaite obtenir de l’aide" : "Je souhaite obtenir de l’aide",
+  "Est ce que je peux recontacter l’aide-bénévole par la suite?" : "Est ce que je peux recontacter l’aide-bénévole par la suite?",
+  "Bien sur ! Le but est de vous faire rencontrer afin de créer une relation de confiance. Vous pouvez donc rappeler l’aide-bénévole quand bon vous semble. Cependant, n’oubliez pas les consignes d’hygiène strictes pour toujours rester en sécurité." : "Bien sur ! Le but est de vous faire rencontrer afin de créer une relation de confiance. Vous pouvez donc rappeler l’aide-bénévole quand bon vous semble. Cependant, n’oubliez pas les consignes d’hygiène strictes pour toujours rester en sécurité.",
+  "Comment savoir qui s’occupe de faire mes courses ?" : "Comment savoir qui s’occupe de faire mes courses ?",
+  "Lors de la récupération de la liste de course, l’aide-bénévole vous contactera sur votre numéro de téléphone fixe ou mobile pour vous informer qu’il est en route vers le magasin. Il vous indiquera aussi quand il est sur le chemin du retour." : "Lors de la récupération de la liste de course, l’aide-bénévole vous contactera sur votre numéro de téléphone fixe ou mobile pour vous informer qu’il est en route vers le magasin. Il vous indiquera aussi quand il est sur le chemin du retour.",
+  "Je n’ai pas la possibilité de faire un versement facilement" : "Je n’ai pas la possibilité de faire un versement facilement",
+  "Vous pouvez demander à un proche de vous aider pour rembourser le bénévole ou télécharger les applications de paiement depuis votre téléphone portable." : "Vous pouvez demander à un proche de vous aider pour rembourser le bénévole ou télécharger les applications de paiement depuis votre téléphone portable.",
+  "Quel est le montant maximal que je peux commander ?" : "Quel est le montant maximal que je peux commander ?",
+  "Comme bon vous semble, mais nous suggérons de ne pas dépasser un montant de 25 à 30€ et de se focaliser sur des biens de première nécessité. Pensez aussi au poids final de votre colis, l’aide bénévole doit pouvoir se déplacer à pied avec." : "Comme bon vous semble, mais nous suggérons de ne pas dépasser un montant de 25 à 30€ et de se focaliser sur des biens de première nécessité. Pensez aussi au poids final de votre colis, l’aide bénévole doit pouvoir se déplacer à pied avec.",
+  "Une initiative 100% bénévole et citoyenne" : "Une initiative 100% bénévole et citoyenne",
+  "Marqué par la tournure des événements et conscients que cette crise nous force à trouver des solutions d’aide pragmatiques, nous avons souhaité par la voie de l’intelligence collective rendre service à la population." : "Marqué par la tournure des événements et conscients que cette crise nous force à trouver des solutions d’aide pragmatiques, nous avons souhaité par la voie de l’intelligence collective rendre service à la population.",
+  "Un mouvement citoyen et non-lucratif initié en ligne par un groupe d’entrepreneurs et professionnels #BeTech qui permet de créer du lien social dans une démarche bienveillante." : "Un mouvement citoyen et non-lucratif initié en ligne par un groupe d’entrepreneurs et professionnels #BeTech qui permet de créer du lien social dans une démarche bienveillante.",
+  "__DUPLICATE_THIS_LINE__" : "__DUPLICATE_THIS_LINE__",
+  "__LEAVE_THIS__" : "__LEAVE_THIS__"
+}';
+
+$data = json_decode($file);
+
+
+echo "<table>";
+
+$i = 0;
+
+foreach ($data as $key => $value) {
+    $i++;
+    echo "<tr><td>ref_".$i."</td><td>$value</td><td>$value</td><td>$value</td></tr>";
+}
+
+echo "</table>";
