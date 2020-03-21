d44e3f0ac2c7001f810e71c0f41e5b163b58a4f0
Gordon Preumont <preumontgr@gmail.com>
2020-03-21 09:33:27 +0100

    layers: data and design

--- components/Cluster.js
+++ components/Cluster.js
@@ -8,6 +8,7 @@ class Cluster extends React.Component {
 
         const { data, type } = this.props;
         const dataSelection = data.filter((item) => item.type === type )
+        const color = type === 'quarantine' ? '#FF7F50' : '#90EE90';
 
         const geojson = {
             type: 'FeatureCollection',
@@ -16,7 +17,40 @@ class Cluster extends React.Component {
                 geometry: { type: 'Point', coordinates: [item.lng, item.lat] }
             })),
         };
+        
+        const clusterLayer = {
+            id: `cluster-layer-${type}`,
+            type: 'circle',
+            filter: ['has', 'point_count'],
+            paint: {
+                'circle-color': color,
+                'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40]
+            }
+        }
 
+        const clusterCountLayer = {
+            id: `cluster-count-${type}`,
+            type: 'symbol',
+            filter: ['has', 'point_count'],
+            layout: {
+                'text-field': '{point_count_abbreviated}',
+                'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                'text-size': 12
+            }
+        };
+
+        const unclusteredPointLayer = {
+            id: `unclustered-point-${type}`,
+            type: 'circle',
+            filter: ['!', ['has', 'point_count']],
+            paint: {
+                'circle-color': color,
+                'circle-radius': 4,
+                'circle-stroke-width': 1,
+                'circle-stroke-color': '#fff'
+            }
+        };
+        
         return (
             <Source
                 type="geojson"
@@ -25,8 +59,9 @@ class Cluster extends React.Component {
                 clusterMaxZoom={14}
                 clusterRadius={50}
             >
-                <Layer />
-
+                <Layer {...clusterLayer} />
+                <Layer {...clusterCountLayer} />
+                <Layer {...unclusteredPointLayer} />
             </Source>
         )
     }
