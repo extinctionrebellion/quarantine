cf3a871567d8108fc14b9eca4505660e1e3d5afd
Gordon Preumont <preumontgr@gmail.com>
2020-03-21 09:24:39 +0100

    component dataset

--- components/Cluster.js
+++ components/Cluster.js
@@ -3,7 +3,20 @@ import { Source, Layer } from 'react-map-gl';
 
 
 class Cluster extends React.Component {
+    
+    render()  {
 
+        const { data, type } = this.props;
+        const dataSelection = data.filter((item) => item.type === type )
+
+        const geojson = {
+            type: 'FeatureCollection',
+            features: dataSelection.map((item) => ({ 
+                type: item.type,
+                geometry: { type: 'Point', coordinates: [item.lng, item.lat] }
+            })),
+        };
+    }
 }
 
 export default Cluster;
